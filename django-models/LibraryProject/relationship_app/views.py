3e55bbb94a122cd48e080809b724396d272fcf82
MzAkorkor <mzakorkor@gmail.com>
2025-09-07 21:27:14 +0000

    Add Library import for class-based view

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
 from django.views.generic import DetailView
-from .models import Book, Library
+from .models import Book, Library 
 
 # Function-based view: list all books
 def list_books(request):
